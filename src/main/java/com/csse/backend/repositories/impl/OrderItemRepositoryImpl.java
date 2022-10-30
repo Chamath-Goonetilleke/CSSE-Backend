96c92de18ce69db6062ae905ecdc3f580ab4fdad
Sunera Jayasinghe <106053595+sunerajayasinghe@users.noreply.github.com>
2022-10-30 23:28:22 +0530

    Fix data fetching error

--- src/main/java/com/csse/backend/repositories/impl/OrderItemRepositoryImpl.java
+++ src/main/java/com/csse/backend/repositories/impl/OrderItemRepositoryImpl.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
@@ -36,9 +35,11 @@ public class OrderItemRepositoryImpl implements OrderItemRepository {
     @Override
     @Query
     public List<OrderItem> getAllCustomerApprovedPurchaseRequisitions(long supplierId) {
-        TypedQuery<OrderItem> query = entityManager
-                .createQuery("SELECT o FROM OrderItem o JOIN FETCH o.suppliers s WHERE s.id = :supplierId AND o.orderStatus = 1", OrderItem.class);
-        query.setParameter("supplierId", supplierId);
+        javax.persistence.Query query = entityManager
+                .createNativeQuery("select i.* \n" +
+                        "from procurement_db.order_item i, procurement_db.order_suppliers s\n" +
+                        "where i.order_item_id = s.order_item_id AND i.order_status = 1 AND s.user_id = ?1", OrderItem.class);
+        query.setParameter(1, supplierId);
         return query.getResultList();
     }
 
